9827c377023a976e1ad9fa19141638550ecf0c70
Konrad <kornisemail@gmail.com>
2023-05-15 15:33:39 +0200

    Add swagger

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,8 +3,9 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 
@@ -13,6 +14,18 @@ async function bootstrap() {
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
+
+  // app.enableCors({
+  //   credentials: true,
+  //   origin: [env.APP_URL, env.API_URL].filter(Boolean),
+  // });
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder().build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(globalPrefix, app, document);
+
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
