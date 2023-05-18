0810843b44f68d22d4db08c587854c27b03154cf
Konrad <kornisemail@gmail.com>
2023-05-18 10:51:35 +0200

    FE: Add login functionality

--- apps/frontend/src/app/components/login/login.component.ts
+++ apps/frontend/src/app/components/login/login.component.ts
@@ -1,8 +1,30 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from '../../services/auth.service';
+import { UsersService } from '../../services/users.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'happy-coding-challenge-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  dialogForm: FormGroup;
+
+  constructor(
+    private authService: AuthenticationService,
+    private usersService: UsersService,
+    private _formBuilder: FormBuilder
+  ) {
+    this.dialogForm = this._formBuilder.group({
+      email: '',
+      password: '',
+    });
+  }
+
+  onFormSubmit() {
+    if (this.dialogForm.valid) {
+      this.authService.login(this.dialogForm.value).subscribe();
+    }
+  }
+}
