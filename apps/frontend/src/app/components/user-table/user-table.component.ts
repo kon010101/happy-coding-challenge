daf608cba924246493634286816644f34120177d
Konrad <kornisemail@gmail.com>
2023-05-18 10:53:03 +0200

    FE: Finish user table

--- apps/frontend/src/app/components/user-table/user-table.component.ts
+++ apps/frontend/src/app/components/user-table/user-table.component.ts
@@ -1,65 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { User } from '@happy-coding-challenge/types';
 import { DialogComponent } from '../dialog/dialog.component';
 import { MatDialog } from '@angular/material/dialog';
-
-const ELEMENT_DATA: User[] = [
-  {
-    id: '1',
-    firstname: 'Konrad',
-    lastname: 'Weiss',
-    email: 'konradweiss@gmx.de',
-    dateOfBirth: new Date(),
-  },
-  {
-    id: '2',
-    firstname: 'Konrad',
-    lastname: 'Weiss',
-    email: 'konradweiss@gmx.de',
-    dateOfBirth: new Date(),
-  },
-  {
-    id: '3',
-    firstname: 'Konrad',
-    lastname: 'Weiss',
-    email: 'konradweiss@gmx.de',
-    dateOfBirth: new Date(),
-  },
-  {
-    id: '4',
-    firstname: 'Konrad',
-    lastname: 'Weiss',
-    email: 'konradweiss@gmx.de',
-    dateOfBirth: new Date(),
-  },
-];
+import { UsersService } from '../../services/users.service';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'happy-coding-challenge-user-table',
   templateUrl: './user-table.component.html',
   styleUrls: ['./user-table.component.scss'],
 })
-export class UserTableComponent {
-  constructor(private _dialog: MatDialog) {}
+export class UserTableComponent implements OnInit {
+  constructor(
+    private _dialog: MatDialog,
+    private _usersService: UsersService
+  ) {}
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
 
   displayedColumns: (keyof User | 'action')[] = [
-    'id',
     'firstname',
     'lastname',
     'email',
     'dateOfBirth',
     'action',
   ];
-  dataSource = ELEMENT_DATA;
+  dataSource!: MatTableDataSource<User>;
 
-  openDialog() {
+  getUsers() {
+    this._usersService.getUsers().subscribe({
+      next: (res: User[]) => {
+        this.dataSource = new MatTableDataSource(res);
+      },
+      error: console.log,
+    });
+  }
+
+  deleteUser(id: string) {
+    this._usersService.deleteUser(id).subscribe({
+      next: (res) => {
+        alert('User deleted');
+        this.getUsers();
+      },
+    });
+  }
+
+  openAddDialog() {
     const dialogRef = this._dialog.open(DialogComponent);
-    // dialogRef.afterClosed().subscribe({
-    //   next: (val) => {
-    //     if (val) {
-    //       this.getEmployeeList();
-    //     }
-    //   },
-    // });
+    dialogRef.afterClosed().subscribe({
+      next: (val) => {
+        if (val) {
+          this.getUsers();
+        }
+      },
+    });
+  }
+
+  openEditDialog(data: User) {
+    const dialogRef = this._dialog.open(DialogComponent, { data });
+    dialogRef.afterClosed().subscribe({
+      next: (val) => {
+        if (val) {
+          this.getUsers();
+        }
+      },
+    });
   }
 }
