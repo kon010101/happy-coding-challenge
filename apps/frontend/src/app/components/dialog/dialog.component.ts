e46fca58b8878d307ebbbca63344569c804a4cfc
Konrad <kornisemail@gmail.com>
2023-05-18 10:51:04 +0200

    FE: Add create/edit to dialog

--- apps/frontend/src/app/components/dialog/dialog.component.ts
+++ apps/frontend/src/app/components/dialog/dialog.component.ts
@@ -1,31 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { MatDialog } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { User } from '@happy-coding-challenge/types';
+import { UsersService } from '../../services/users.service';
 
 @Component({
   selector: 'happy-coding-challenge-dialog',
   templateUrl: './dialog.component.html',
   styleUrls: ['./dialog.component.scss'],
 })
-export class DialogComponent {
+export class DialogComponent implements OnInit {
   dialogForm: FormGroup;
 
-  constructor(private _dialog: MatDialog, private _formBuilder: FormBuilder) {
+  constructor(
+    private _formBuilder: FormBuilder,
+    private _usersService: UsersService,
+    private _dialogRef: MatDialogRef<DialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public userData: User
+  ) {
     this.dialogForm = this._formBuilder.group({
       firstname: '',
       lastname: '',
       email: '',
       dateOfBirth: '',
+      password: '',
     });
   }
 
-  closeDialog() {
-    this._dialog.closeAll();
+  ngOnInit(): void {
+    this.dialogForm.patchValue(this.userData);
   }
 
   onFormSubmit() {
-    if (this.dialogForm.valid) {
-      console.log(this.dialogForm.value);
+    if (this.userData) {
+      this._usersService
+        .updateUser(this.userData.id, this.dialogForm.value)
+        .subscribe({
+          next: (val: Partial<User>) => {
+            console.log({ val });
+            alert('Updated');
+            this._dialogRef.close(true);
+          },
+          error: (err: Partial<User>) => {
+            console.log(err);
+          },
+        });
+    } else {
+      if (this.dialogForm.valid) {
+        this._usersService.createUser(this.dialogForm.value).subscribe({
+          next: (val: Partial<User>) => {
+            alert('User added');
+            this._dialogRef.close(true);
+          },
+          error: (err: Partial<User>) => {
+            console.log(err);
+          },
+        });
+      }
     }
   }
 }
