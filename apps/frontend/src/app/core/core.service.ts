56ea5a5d35ec2c2a5fa202632fbaae69e751ea21
Konrad <kornisemail@gmail.com>
2023-05-19 13:37:41 +0200

    Fix snackbar

--- apps/frontend/src/app/core/core.service.ts
+++ apps/frontend/src/app/core/core.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
-  providedIn: 'any',
+  providedIn: 'root',
 })
 export class CoreService {
   constructor(private _snackBar: MatSnackBar) {}
